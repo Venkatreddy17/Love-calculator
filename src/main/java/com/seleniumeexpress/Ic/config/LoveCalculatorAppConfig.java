38ca931ebec28a9679ea3d01d4138cae02ee9ff1
Shivannagarivenkat1706@gmail.com <shivannagarivenkat1706@gmail.com>
2024-10-21 22:28:18 +0530

    add viewResolver in LoveCalculatorAppConfig class

--- src/main/java/com/seleniumeexpress/Ic/config/LoveCalculatorAppConfig.java
+++ src/main/java/com/seleniumeexpress/Ic/config/LoveCalculatorAppConfig.java
@@ -1,10 +1,22 @@
 package com.seleniumeexpress.Ic.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
 @ComponentScan(basePackages = "com.seleniumeexpress.Ic.controllers")
 public class LoveCalculatorAppConfig {
-
+     
+	//set up view Resolver
+  @Bean
+  InternalResourceViewResolver	viewResolver(){
+		InternalResourceViewResolver viewResolver=new InternalResourceViewResolver();
+		viewResolver.setPrefix("/WEB-INF/view/");
+		viewResolver.setSuffix(".jsp");
+		
+		return viewResolver;
+	}
+  
 }
